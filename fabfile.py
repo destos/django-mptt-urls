c25f3b20bc769dae89e7364d46850725163f66b8
Patrick Forringer <patrick@forringer.com>
2014-05-01 09:31:32 -0500

    adding support for travis and local testing
    * testing using tox for django 1.5, 1.6 and a fabric test runner
    * moving test_project to test_app
    * adding support to test app coverage as well.

---
+++ fabfile.py
@@ -0,0 +1,35 @@
+"""
+Creating standalone Django apps is a PITA because you're not in a project, so
+you don't have a settings.py file.  I can never remember to define
+DJANGO_SETTINGS_MODULE, so I run these commands which get the right env
+automatically.
+"""
+import functools
+import os
+
+from fabric.api import local as _local
+
+
+NAME = os.path.basename(os.path.dirname(__file__))
+ROOT = os.path.abspath(os.path.dirname(__file__))
+APP_NAME = 'mptt_urls'
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test_app.test_app.settings'
+os.environ['PYTHONPATH'] = os.pathsep.join([ROOT, ])
+
+_local = functools.partial(_local, capture=False)
+
+
+def shell():
+    """Start a Django shell with the test settings."""
+    _local('django-admin.py shell')
+
+
+def test(args=''):
+    """Run the test suite."""
+    _local('django-admin.py test %s' % args)
+
+
+def test_coverage():
+    _local('coverage run --source=%s --omit=*/migrations/*.py '
+           '$(which django-admin.py) test' % APP_NAME)
---
+++ test_app/test_app/settings.py
@@ -0,0 +1,89 @@
+"""
+Django settings for test_app project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'b)7ui$fa-_mwcxc6u6=@9ff0bgnn)hliq=(lij1ztukzm_%$2_'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+ROOT_URLCONF = 'test_app.urls'
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_nose',
+    'test_app.test_app',
+    'mptt',
+    'test_app.gallery',
+)
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = ('--nocapture', )
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = False
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+CACHES = dict(
+    default=dict(BACKEND='django.core.cache.backends.dummy.DummyCache'))
